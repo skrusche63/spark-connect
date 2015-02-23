aa8112941863526c7a6397da92a86a82146602da
skrusche63 <skrusche63@googlemail.com>
2015-02-23 13:03:12 +0100

    Request parameters no longer contain schema info.

--- src/main/scala/de/kp/spark/connect/SQLSource.scala
+++ src/main/scala/de/kp/spark/connect/SQLSource.scala
@@ -46,7 +46,7 @@ object Sources {
   val PARQUET:String = "parquet"
     
 }
-// TODO schema & params should be harmonized
+
 class SQLSource(
     @transient sqlContext:SQLContext,
     config:ConnectConfig,
@@ -61,7 +61,7 @@ class SQLSource(
    */
   private val names = sqlContext.sparkContext.broadcast(schema.fieldNames)
 
-  private val rowRDD = getRDD(source,params).map(rec => {
+  private val rowRDD = getRDD.map(rec => {
     val values = names.value.map(name => rec(name))
     Row.fromSeq(values)
   })
@@ -74,22 +74,64 @@ class SQLSource(
   
   def executeQuery(query:String):SchemaRDD = sqlContext.sql(query)
   
-  private def getRDD(source:String,params:Map[String,String]):RDD[Map[String,Any]] = {
+  private def getRDD:RDD[Map[String,Any]] = {
     
     val sc = sqlContext.sparkContext
+    val columns = schema.fieldNames
     
     source match {
       
-      case Sources.AEROSPIKE => new AerospikeSource(sc).read(config,params)
-      case Sources.CASSANDRA => new CassandraSource(sc).read(config,params)
-
+      case Sources.AEROSPIKE => {
+        /*
+         * Column names are called bin names in the 
+         * terminology of Aerospike
+         */
+        val req_params = params ++ Map("binnames" -> columns.mkString(","))
+        new AerospikeSource(sc).read(config,req_params)
+        
+      }
+      case Sources.CASSANDRA => {
+        
+        val req_params = params ++ Map("columns" -> columns.mkString(","))
+        new CassandraSource(sc).read(config,req_params)
+      
+      }
       case Sources.ELASTICSEARCH => new ElasticSource(sc).read(config,params)
-      case Sources.HBASE => new HBaseSource(sc).read(config,params)
+      case Sources.HBASE => {
+ 
+        val types = schema.fields.map(field => {
+          
+          field.dataType match {
+            
+            case DoubleType => "double"
+            case IntegerType => "integer"
+            
+            case LongType => "long"
+            case StringType => "string"
+              
+            case _ => throw new Exception("Data type is not supported.")
+          }
+          
+        })
 
-      case Sources.JDBC => new JdbcSource(sc).read(config,params)
+        val req_params = params ++ Map("names" -> columns.mkString(","), "types" -> types.mkString(","))
+        new HBaseSource(sc).read(config,req_params)
+      
+      }
+      case Sources.JDBC => {
+      
+        val req_params = params ++ Map("fields" -> columns.mkString(","))
+        new JdbcSource(sc).read(config,req_params)
+      
+      }
       case Sources.MONGODB => new MongoSource(sc).read(config,params)
 
-      case Sources.PARQUET => new ParquetSource(sc).read(config,params)
+      case Sources.PARQUET => {
+      
+        val req_params = params ++ Map("fields" -> columns.mkString(","))
+        new ParquetSource(sc).read(config,params)
+      
+      }
       
       case _ => throw new Exception(String.format("""Data source %s is not supported.""",source))
       
